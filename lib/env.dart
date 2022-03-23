4e00d4f8684339ea8ae09948cc72f05d4f47711c
angelo-squadra <angelo.savioti@squadra.com.br>
2022-03-23 17:34:18 -0300

    android setup

---
+++ lib/env.dart
@@ -0,0 +1,15 @@
+class Env {
+  static const String googleFirebasePublicName = "project-953645021666";
+
+  static const String firebaseWebClientId = "953645021666-0uan4eaqccnqcmebqtim6dr9dj11gr9k.apps.googleusercontent.com";
+
+  static const String firebaseWebClientSecret =
+      "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";
+
+  static const String facebookAppID = "704104207438457";
+
+  static const String facebookAppSecret = "b4542175388293e1d47658e8bc2e7113";
+
+  static const String androidKeySHA1 =
+      "51:DB:28:5C:35:3B:A9:4E:74:E4:25:C6:95:47:F9:9D:67:80:D8:30";
+}
